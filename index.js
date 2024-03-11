9d949a0373ee5955f879ac3f33728de947ae4a7e
ashok parihar <aparihar@email.arizona.edu>
2024-03-10 23:05:39 -0600

    Coding the function that creates the SVG file after checking the user inputs from the questions.

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 // Including the packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-// Importing classes from ./lib/shapes directory for our module shapes
+// Importing classes from ./lib/shapes directory for our module shapes.
 // const { Triangle, Square, Circle } = require("./lib/shapes");
+
+// Using the answers from the user, the function writeToFile creates the SVG file.
+function writeToFile(fileName, answers) {
+    // Variable starts as an empty string.
+    var svgString = "";
+    // Setting width and height as asked for in the Acceptance Criteria.
+    // Used the site https://www.w3.org/TR/2011/REC-SVG11-20110816/intro.html#NamespaceAndDTDIdentifiers to learn about SVG namespace and identifiers.
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    // The following tag ensures that the user font will be on top of the shape, not the opposite.
+    svgString += "<g>";
+    // Takes user answer for which shape they want and inserts it into SVG file.
+    svgString += `${answers.shape}`;
+  
+    // If-else statement adds shape/polygon properties to the svgString from the user input for what shape they chose.
+    var shapeChoice;
+    if (answers.shape === "Triangle") {
+      shapeChoice = new Triangle();
+    // Polygon properties given in the Getting Started Section of the Assignment
+      svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "Square") {
+      shapeChoice = new Square();
+    // The x position of the top left corner of the rectangle.
+    // The y position of the top left corner of the rectangle.
+      svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+      shapeChoice = new Circle();
+    // The x position of the center of the circle.
+    // The y position of the center of the circle.
+      svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+  
+    // The x and y attributes determine where in the viewport the text will appear. 
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    // Closing our tags.
+    svgString += "</g>";
+    svgString += "</svg>";
+  
+    // Using writeFile to generate an SVG file.
+    // This takes in the file name given in the promptUser function, the svgString, and a ternary operator which handles logging any errors, or a "Created our logo.svg" message to the console  
+    fs.writeFile(fileName, svgString, (err) => {
+      err ? console.log(err) : console.log("Created our logo.svg");
+    });
+  }
 
 // The function uses .prompt to collect answers to the following questions from the user in the command line.
 function promptUser() {
@@ -37,7 +80,7 @@ function promptUser() {
           promptUser();
         } else {
           // Calling the write file function to generate an SVG file
-          writeToFile("logo.svg", answers);
+        //   writeToFile("logo.svg", answers);
         }
       });
   }
